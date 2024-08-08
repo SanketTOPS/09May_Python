3cece9ad8bde881ba124499957190c8a405c584a
SanketTops <sanket.tops@gmail.com>
2024-08-08 12:20:16 +0530

    DBApp - Update and Delete

--- Django Project/NewdbProject/myapp/views.py
+++ Django Project/NewdbProject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import *
 
 # Create your views here.
@@ -16,3 +16,21 @@ def index(request):
 def showdata(request):
     data=studinfo.objects.all()
     return render(request,'showdata.html',{'data':data})
+
+
+def deletedata(request,id):
+    stid=studinfo.objects.get(id=id)
+    studinfo.delete(stid)
+    return redirect('showdata')
+
+def updatedata(request,id):
+    stid=studinfo.objects.get(id=id)
+    if request.method=='POST':
+        newreq=updateForm(request.POST,instance=stid)
+        if newreq.is_valid(): #TRUE
+            newreq.save()
+            print("Record updated!")
+            return redirect('showdata')
+        else:
+            print(newreq.errors)
+    return render(request,'updatedata.html',{'stid':stid})
