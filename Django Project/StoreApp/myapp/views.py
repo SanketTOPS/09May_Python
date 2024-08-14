c74bd3df62fe75598d95caff5a6c39878dbe2f15
SanketTops <sanket.tops@gmail.com>
2024-08-14 16:46:46 +0530

    Product API

---
+++ Django Project/StoreApp/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+def index(request):
+    url="https://fakestoreapi.com/products"
+    req=requests.get(url)
+    data=req.json()
+    return render(request,'index.html',{'data':data})
