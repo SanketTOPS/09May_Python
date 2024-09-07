31483d827f48e28f5d239f81c697157fe7869e52
SanketTops <sanket.tops@gmail.com>
2024-09-07 12:46:16 +0530

    Error Pages

--- Django Project/FinalProject/FinalProject/settings.py
+++ Django Project/FinalProject/FinalProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$slbx^g2x2$2u24#aadw8+1!ur%76^5l046rn+!h(%+l%xth8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
